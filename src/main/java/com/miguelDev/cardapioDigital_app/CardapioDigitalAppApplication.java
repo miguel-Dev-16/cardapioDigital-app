6b368168ef972889c719ea6f921dbae486a7eedc
miguel-Dev-16 <miguelanalista7@gmail.com>
2024-12-27 16:58:49 -0300

    Criação do Projeto Da Issue RCD002

---
+++ src/main/java/com/miguelDev/cardapioDigital_app/CardapioDigitalAppApplication.java
@@ -0,0 +1,13 @@
+package com.miguelDev.cardapioDigital_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardapioDigitalAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardapioDigitalAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miguelDev/cardapioDigital_app/CardapioDigitalAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miguelDev.cardapioDigital_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardapioDigitalAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
